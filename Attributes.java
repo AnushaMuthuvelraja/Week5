f7b3fe18fb46f6c63b7396a14ae54e1f40a3984e
AnushaMuthuvelraja <98222036+AnushaMuthuvelraja@users.noreply.github.com>
2022-02-28 00:05:16 +0000

    Update Attributes.java

--- Attributes.java
+++ Attributes.java
@@ -55,3 +55,20 @@ public class Attributes extends  BaseClassAttributes
 		driver.findElement(By.name("submitButton")).click();
 	}
 }
+
+------------------------------------------------------------
+
+//Attributestestng.xml
+
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE suite SYSTEM "https://testng.org/testng-1.0.dtd">
+<suite name="Suite">
+  <test thread-count="5" name="Test">
+    <classes>
+      <class name="Week5.EditLeadTest"/>
+      <class name="Week5.DuplicateLeadTest"/>
+      <class name="Week5.DeleteLeadTest"/>
+      <class name="Week5.Lead"/>
+    </classes>
+  </test> <!-- Test -->
+</suite> <!-- Suite -->
