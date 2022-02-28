b8640301a1c26bbab99b29f37cb23664f0e76c8a
AnushaMuthuvelraja <98222036+AnushaMuthuvelraja@users.noreply.github.com>
2022-02-28 01:22:50 +0000

    Update DeleteIncident.java

--- DeleteIncident.java
+++ DeleteIncident.java
@@ -35,3 +35,59 @@ public class DeleteIncident extends BaseClassServiceNow{
 
 }
 
+-------------------------------------------------------------------------------------------------
+ BaseClassServiceNow
+ 
+package Homework.week5;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseClassServiceNow 
+{
+	public ChromeDriver driver;
+	
+	
+  @BeforeMethod
+	public void preCondition() throws InterruptedException
+	{
+		WebDriverManager.chromedriver().setup();
+		 driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://dev35942.service-now.com/");
+		Thread.sleep(10000);
+		driver.switchTo().frame("gsft_main");		
+		driver.findElement(By.xpath("//input[@id='user_name']")).sendKeys("admin");
+		driver.findElement(By.xpath("//input[@id='user_password']")).sendKeys("London@2014");
+		driver.findElement(By.xpath("//button[@id='sysverb_login']")).click();
+		driver.findElement(By.xpath("//input[@id='filter']")).sendKeys("incident");//search indcident
+		driver.findElement(By.xpath("//input[@id='filter']")).sendKeys(Keys.ENTER);//enter
+	}
+	
+	/*
+	 * @AfterMethod public void postCondition() { driver.close(); }
+	 */
+}
+----------------------------------------------------------------------------------------------------------------
+	//testng.xml
+	
+	<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE suite SYSTEM "https://testng.org/testng-1.0.dtd">
+<suite name="Suite" verbose="5">
+  <test thread-count="5" name="Test">
+    <classes>
+      
+      <class name="Homework.week5.DeleteIncident"/>
+      
+     
+       
+    </classes>
+  </test> <!-- Test -->
+</suite> <!-- Suite -->
+
+
